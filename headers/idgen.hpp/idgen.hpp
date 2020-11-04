1096c4ea75dca3337d5b6dd9a894e03d1a0c052f
BlackMATov <blackmat@mail.ru>
2020-11-05 02:05:53 +0700

    basic basic_id

--- headers/idgen.hpp/idgen.hpp
+++ headers/idgen.hpp/idgen.hpp
@@ -6,6 +6,124 @@
 
 #pragma once
 
+#include <cassert>
+#include <cstddef>
+#include <cstdint>
+
 namespace idgen_hpp
 {
+    template < typename Value >
+    class id_value_traits;
+
+    template <>
+    class id_value_traits<std::uint8_t> {
+    public:
+        static constexpr int index_bits = 5;
+        static constexpr int version_bits = 3;
+    };
+
+    template <>
+    class id_value_traits<std::uint16_t> {
+    public:
+        static constexpr int index_bits = 10;
+        static constexpr int version_bits = 6;
+    };
+
+    template <>
+    class id_value_traits<std::uint32_t> {
+    public:
+        static constexpr int index_bits = 20;
+        static constexpr int version_bits = 12;
+    };
+
+    template <>
+    class id_value_traits<std::uint64_t> {
+    public:
+        static constexpr int index_bits = 40;
+        static constexpr int version_bits = 24;
+    };
+}
+
+namespace idgen_hpp
+{
+    template < typename Tag
+             , typename Value
+             , typename Traits = id_value_traits<Value> >
+    class basic_id {
+    public:
+        using tag_type = Tag;
+        using value_type = Value;
+        using traits_type = Traits;
+    public:
+        static constexpr int index_bits = static_cast<int>(Traits::index_bits);
+        static constexpr int version_bits = static_cast<int>(Traits::version_bits);
+
+        static constexpr value_type index_mask = (value_type(1) << index_bits) - value_type(1);
+        static constexpr value_type version_mask = (value_type(1) << version_bits) - value_type(1);
+    public:
+        basic_id() = default;
+
+        explicit basic_id(value_type index)
+        : value_(index & index_mask) {
+            assert(index <= index_mask);
+        }
+
+        basic_id(value_type index, value_type version)
+        : value_((index & index_mask) | ((version & version_mask) << index_bits)) {
+            assert(index <= index_mask);
+            assert(version <= version_mask);
+        }
+
+        value_type value() const noexcept {
+            return value_;
+        }
+
+        value_type index() const noexcept {
+            return value_ & index_mask;
+        }
+
+        value_type version() const noexcept {
+            return (value_ >> index_bits) & version_mask;
+        }
+    private:
+        value_type value_{};
+    };
+}
+
+namespace idgen_hpp
+{
+    template < typename Tag, typename Value, typename Traits >
+    bool operator<(basic_id<Tag, Value, Traits> l, basic_id<Tag, Value, Traits> r) noexcept {
+        return l.value() < r.value();
+    }
+
+    template < typename Tag, typename Value, typename Traits >
+    bool operator<=(basic_id<Tag, Value, Traits> l, basic_id<Tag, Value, Traits> r) noexcept {
+        return l.value() <= r.value();
+    }
+
+    template < typename Tag, typename Value, typename Traits >
+    bool operator==(basic_id<Tag, Value, Traits> l, basic_id<Tag, Value, Traits> r) noexcept {
+        return l.value() == r.value();
+    }
+
+    template < typename Tag, typename Value, typename Traits >
+    bool operator!=(basic_id<Tag, Value, Traits> l, basic_id<Tag, Value, Traits> r) noexcept {
+        return l.value() != r.value();
+    }
+}
+
+namespace idgen_hpp
+{
+    template < typename Tag >
+    using id8 = basic_id<Tag, std::uint8_t>;
+
+    template < typename Tag >
+    using id16 = basic_id<Tag, std::uint16_t>;
+
+    template < typename Tag >
+    using id32 = basic_id<Tag, std::uint32_t>;
+
+    template < typename Tag >
+    using id64 = basic_id<Tag, std::uint64_t>;
 }
--- untests/idgen_tests.cpp
+++ untests/idgen_tests.cpp
@@ -14,6 +14,97 @@ namespace
 {
 }
 
-TEST_CASE("idgen") {
-    REQUIRE(true);
+TEST_CASE("idgen")
+{
+    SECTION("id8") {
+        using id8 = idgen::id8<struct untagged>;
+        STATIC_REQUIRE(sizeof(id8) == 1u);
+        STATIC_REQUIRE(id8::index_bits == 5u);
+        STATIC_REQUIRE(id8::version_bits == 3u);
+        STATIC_REQUIRE(id8::index_mask == 31u);
+        STATIC_REQUIRE(id8::version_mask == 7u);
+    }
+
+    SECTION("id16") {
+        using id16 = idgen::id16<struct untagged>;
+        STATIC_REQUIRE(sizeof(id16) == 2u);
+        STATIC_REQUIRE(id16::index_bits == 10u);
+        STATIC_REQUIRE(id16::version_bits == 6u);
+        STATIC_REQUIRE(id16::index_mask == 1023u);
+        STATIC_REQUIRE(id16::version_mask == 63u);
+    }
+
+    SECTION("id32") {
+        using id32 = idgen::id32<struct untagged>;
+        STATIC_REQUIRE(sizeof(id32) == 4u);
+        STATIC_REQUIRE(id32::index_bits == 20u);
+        STATIC_REQUIRE(id32::version_bits == 12u);
+        STATIC_REQUIRE(id32::index_mask == 1048575u);
+        STATIC_REQUIRE(id32::version_mask == 4095u);
+    }
+
+    SECTION("id64") {
+        using id64 = idgen::id64<struct untagged>;
+        STATIC_REQUIRE(sizeof(id64) == 8u);
+        STATIC_REQUIRE(id64::index_bits == 40u);
+        STATIC_REQUIRE(id64::version_bits == 24u);
+        STATIC_REQUIRE(id64::index_mask == 1099511627775ull);
+        STATIC_REQUIRE(id64::version_mask == 16777215ull);
+    }
+
+    SECTION("basic_id") {
+        using id8 = idgen::id8<struct untagged>;
+        {
+            id8 id;
+            REQUIRE(id.value() == 0b000'00000);
+            REQUIRE(id.index() == 0u);
+            REQUIRE(id.version() == 0u);
+        }
+        {
+            id8 id(31u);
+            REQUIRE(id.value() == 0b000'11111);
+            REQUIRE(id.index() == 31u);
+            REQUIRE(id.version() == 0u);
+        }
+        {
+            id8 id(21u, 2u);
+            REQUIRE(id.value() == 0b010'10101);
+            REQUIRE(id.index() == 21u);
+            REQUIRE(id.version() == 2u);
+        }
+        {
+            REQUIRE(id8() == id8());
+            REQUIRE(id8(1, 3) == id8(1, 3));
+
+            REQUIRE_FALSE(id8(1, 3) == id8(1, 4));
+            REQUIRE_FALSE(id8(1, 3) == id8(2, 3));
+            REQUIRE_FALSE(id8(1, 3) == id8(2, 4));
+        }
+        {
+            REQUIRE_FALSE(id8() != id8());
+            REQUIRE_FALSE(id8(1, 3) != id8(1, 3));
+
+            REQUIRE(id8(1, 3) != id8(1, 4));
+            REQUIRE(id8(1, 3) != id8(2, 3));
+            REQUIRE(id8(1, 3) != id8(2, 4));
+        }
+        {
+            REQUIRE_FALSE(id8(1, 3) < id8(1, 3));
+            REQUIRE(id8(1, 3) <= id8(1, 3));
+
+            REQUIRE(id8(1, 3) < id8(2, 3));
+            REQUIRE(id8(1, 3) < id8(1, 4));
+            REQUIRE(id8(2, 3) < id8(1, 4));
+            REQUIRE(id8(1, 3) <= id8(2, 3));
+            REQUIRE(id8(1, 3) <= id8(1, 4));
+            REQUIRE(id8(2, 3) <= id8(1, 4));
+
+            REQUIRE_FALSE(id8(2, 3) < id8(1, 3));
+            REQUIRE_FALSE(id8(1, 4) < id8(1, 3));
+            REQUIRE_FALSE(id8(1, 4) < id8(2, 3));
+            REQUIRE_FALSE(id8(2, 3) <= id8(1, 3));
+            REQUIRE_FALSE(id8(1, 4) <= id8(1, 3));
+            REQUIRE_FALSE(id8(1, 4) <= id8(2, 3));
+        }
+    }
 }
