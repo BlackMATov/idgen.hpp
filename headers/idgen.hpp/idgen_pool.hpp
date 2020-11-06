b260b4b64cc23da8da1be50e3a136d547c4891e2
BlackMATov <blackmat@mail.ru>
2020-11-07 00:46:39 +0700

    basic id pool

---
+++ headers/idgen.hpp/idgen_pool.hpp
@@ -0,0 +1,94 @@
+/*******************************************************************************
+ * This file is part of the "https://github.com/blackmatov/idgen.hpp"
+ * For conditions of distribution and use, see copyright notice in LICENSE.md
+ * Copyright (C) 2020, by Matvey Cherevko (blackmatov@gmail.com)
+ ******************************************************************************/
+
+#pragma once
+
+#include <cassert>
+#include <cstddef>
+#include <cstdint>
+
+#include <stdexcept>
+#include <utility>
+#include <vector>
+
+#include "idgen_id.hpp"
+
+namespace idgen_hpp
+{
+    template < typename Tag
+             , typename Value
+             , typename Traits = id_value_traits<Value> >
+    class pool {
+    public:
+        using id_type = id<Tag, Value, Traits>;
+        using id_value_type = typename id_type::value_type;
+        using id_traits_type = typename id_type::traits_type;
+    public:
+        pool() = default;
+
+        pool(pool&& other) = default;
+        pool& operator=(pool&& other) = default;
+
+        pool(const pool& other) = default;
+        pool& operator=(const pool& other) = default;
+
+        id_type acquire() {
+            if ( available_ != id_type::index_mask ) {
+                const auto index = available_;
+                const auto version = acquired_ids_[index].version();
+                available_ = acquired_ids_[index].index();
+                return acquired_ids_[index] = id_type(index, version);
+            }
+
+            if ( acquired_ids_.size() < id_type::index_mask ) {
+                return acquired_ids_.emplace_back(
+                    static_cast<id_value_type>(acquired_ids_.size()),
+                    0u);
+            }
+
+            throw std::logic_error("idgen_hpp::pool (id index overflow)");
+        }
+
+        void release(id_type id) noexcept {
+            assert(valid(id));
+            acquired_ids_[id.index()] = id_type(
+                available_ & id_type::index_mask,
+                (id.version() + 1u) & id_type::version_mask);
+            available_ = id.index();
+        }
+
+        bool valid(id_type id) const noexcept {
+            return id.index() < acquired_ids_.size()
+                && acquired_ids_[id.index()] == id;
+        }
+
+        std::size_t alive() const noexcept {
+            std::size_t result = acquired_ids_.size();
+            for ( auto i = available_; i != id_type::index_mask; --result ) {
+                i = acquired_ids_[i].index();
+            }
+            return result;
+        }
+    private:
+        std::vector<id_type> acquired_ids_;
+        id_value_type available_{id_type::index_mask};
+    };
+}
+
+namespace idgen_hpp
+{
+    template < typename Tag >
+    using id8_pool = pool<Tag, std::uint8_t>;
+
+    template < typename Tag >
+    using id16_pool = pool<Tag, std::uint16_t>;
+
+    template < typename Tag >
+    using id32_pool = pool<Tag, std::uint32_t>;
+
+    template < typename Tag >
+    using id64_pool = pool<Tag, std::uint64_t>;
+}
---
+++ untests/idgen_pool_tests.cpp
@@ -0,0 +1,145 @@
+/*******************************************************************************
+ * This file is part of the "https://github.com/blackmatov/idgen.hpp"
+ * For conditions of distribution and use, see copyright notice in LICENSE.md
+ * Copyright (C) 2020, by Matvey Cherevko (blackmatov@gmail.com)
+ ******************************************************************************/
+
+#define CATCH_CONFIG_FAST_COMPILE
+#include <catch2/catch.hpp>
+
+#include <idgen.hpp/idgen_pool.hpp>
+namespace idgen = idgen_hpp;
+
+TEST_CASE("idgen_pool")
+{
+    SECTION("acquire/release") {
+        using id8 = idgen::id8<struct untagged>;
+        using id8_pool = idgen::id8_pool<struct untagged>;
+        {
+            id8_pool p;
+
+            const id8 i0 = p.acquire();
+            REQUIRE(i0 == id8(0u, 0u));
+
+            const id8 i1 = p.acquire();
+            REQUIRE(i1 == id8(1u, 0u));
+
+            const id8 i2 = p.acquire();
+            REQUIRE(i2 == id8(2u, 0u));
+        }
+        {
+            id8_pool p;
+
+            const id8 i0_0 = p.acquire();
+            REQUIRE(i0_0 == id8(0u, 0u));
+
+            p.release(i0_0);
+
+            const id8 i0_1 = p.acquire();
+            REQUIRE(i0_1 == id8(0u, 1u));
+
+            p.release(i0_1);
+
+            const id8 i0_2 = p.acquire();
+            REQUIRE(i0_2 == id8(0u, 2u));
+        }
+        {
+            id8_pool p;
+
+            std::vector<id8> ids;
+
+            for ( id8::value_type i = 0; i < 31; ++i ) {
+                ids.push_back(p.acquire());
+                REQUIRE(ids.back() == id8(i, 0u));
+            }
+
+            for ( id8 id : ids ) {
+                REQUIRE(p.valid(id));
+            }
+
+            REQUIRE_THROWS_AS(p.acquire(), std::logic_error);
+
+            for ( id8 id : ids ) {
+                p.release(id);
+            }
+
+            for ( id8 id : ids ) {
+                REQUIRE_FALSE(p.valid(id));
+            }
+
+            ids.clear();
+
+            for ( id8::value_type i = 0; i < 31; ++i ) {
+                ids.push_back(p.acquire());
+                REQUIRE(ids.back() == id8(30u - i, 1u));
+            }
+
+            for ( id8 id : ids ) {
+                REQUIRE(p.valid(id));
+            }
+
+            REQUIRE_THROWS_AS(p.acquire(), std::logic_error);
+        }
+    }
+
+    SECTION("valid") {
+        using id8 = idgen::id8<struct untagged>;
+        using id8_pool = idgen::id8_pool<struct untagged>;
+        {
+            id8_pool p;
+
+            const id8 i0_0 = p.acquire();
+            REQUIRE(i0_0 == id8(0u, 0u));
+            REQUIRE(p.valid(i0_0));
+
+            p.release(i0_0);
+            REQUIRE_FALSE(p.valid(i0_0));
+
+            const id8 i0_1 = p.acquire();
+            REQUIRE(i0_1 == id8(0u, 1u));
+            REQUIRE(p.valid(i0_1));
+            REQUIRE_FALSE(p.valid(i0_0));
+
+            p.release(i0_1);
+            REQUIRE_FALSE(p.valid(i0_0));
+            REQUIRE_FALSE(p.valid(i0_1));
+        }
+    }
+
+    SECTION("alive") {
+        using id8 = idgen::id8<struct untagged>;
+        using id8_pool = idgen::id8_pool<struct untagged>;
+        {
+            id8_pool p;
+            REQUIRE(p.alive() == 0u);
+
+            {
+                id8 i1 = p.acquire();
+                REQUIRE(p.alive() == 1u);
+
+                p.release(i1);
+                REQUIRE(p.alive() == 0u);
+            }
+
+            {
+                id8 i1 = p.acquire();
+                REQUIRE(p.alive() == 1u);
+
+                id8 i2 = p.acquire();
+                REQUIRE(p.alive() == 2u);
+
+                p.release(i2);
+                REQUIRE(p.alive() == 1u);
+
+                i2 = p.acquire();
+                REQUIRE(p.alive() == 2u);
+
+                p.release(i1);
+                REQUIRE(p.alive() == 1u);
+
+                p.release(i2);
+                REQUIRE(p.alive() == 0u);
+            }
+        }
+    }
+}
